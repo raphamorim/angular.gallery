f87e58951a69912159837e9c2b265e139accf2e1
Raphael Amorim <rapha850@gmail.com>
2014-07-10 23:04:07 -0300

    include main.js and angular to this project

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+var gallery = angular.module('gallery', [
+    'myApp.controllers',
+    'myApp.filters',
+    'myApp.services',
+    'myApp.directives'
+    // ,'ngRoute'
+]);
