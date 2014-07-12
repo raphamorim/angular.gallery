fb216f5b6894fec751743e9c63bdd2c99dcb5bbd
Raphael Amorim <rapha850@gmail.com>
2014-07-12 00:36:04 -0300

    using angular to build and get video gallery

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -4,6 +4,16 @@
 
 angular.module('myApp.controllers', [])
 
-.controller('a', function($scope) {
-
+.controller('Main', function($scope, $http, $sce) {
+    $http({method: 'GET', url: 'gallery/gallery.json'})
+    .success(function(data) {
+        $scope.apps = data.apps,
+        $scope.random = Math.floor(Math.random() * $scope.apps.length),
+        $scope.mainApp = {
+          name: $scope.apps[$scope.random].name,
+          author: $scope.apps[$scope.random].author,
+          url: $sce.trustAsResourceUrl($scope.apps[$scope.random].url),
+          repository: $scope.apps[$scope.random].repository
+        };
+    });
 })
